0c57fda155b150c83aa5b50817934d1a69121583
Mamtha Kashyap <mamtha@Mamthas-MacBook-Air.local>
2015-03-29 18:11:58 -0700

    Program Structure

---
+++ MyMedico/src/main/java/com/sjsu/MyMedico/App.java
@@ -0,0 +1,13 @@
+package com.sjsu.MyMedico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
